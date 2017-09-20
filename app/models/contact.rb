04fb406b2e1c57349282adfc02368285aa8fa0f2
MrFuentastic <vfuentes8587@gmail.com>
2017-09-19 21:41:18 -0500

    added groups and a join table

--- app/models/contact.rb
+++ app/models/contact.rb
@@ -1,4 +1,6 @@
 class Contact < ApplicationRecord
+has_many :contact_groups
+has_many :groups, through: :contact_groups
 
   def friendly_updated_at
     updated_at.strftime("%A, %d %b %Y %l:%M %p")
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,16 +6,34 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-100.times do
-  first_name = Faker::Name.first_name
-  last_name = Faker::Name.last_name
+# 100.times do
+#   first_name = Faker::Name.first_name
+#   last_name = Faker::Name.last_name
 
-  contact = Contact.new(
-                        name: first_name + last_name,
-                        phone_number: Faker::PhoneNumber.phone_number,
-                        email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
-                        address: Faker::Address.street_address,
-                        )
+#   contact = Contact.new(
+#                         name: first_name + last_name,
+#                         phone_number: Faker::PhoneNumber.phone_number,
+#                         email: Faker::Internet.free_email("#{first_name}.#{last_name}"),
+#                         address: Faker::Address.street_address,
+#                         )
 
-  contact.save
-end
+#   contact.save
+# end
+
+Group.create!(name: "Friends")
+Group.create!(name: "Family")
+Group.create!(name: "Ignore")
+Group.create!(name: "Chicago")
+Group.create!(name: "Tucson")
+Group.create!(name: "Business")
+Group.create!(name: "New York")
+
+
+ContactGroup.create!(contact_id: 2, group_id: 1)
+ContactGroup.create!(contact_id: 2, group_id: 3)
+ContactGroup.create!(contact_id: 1, group_id: 1)
+ContactGroup.create!(contact_id: 1, group_id: 7)
+ContactGroup.create!(contact_id: 5, group_id: 6)
+ContactGroup.create!(contact_id: 5, group_id: 7)
+
+
